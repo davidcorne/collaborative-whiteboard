efc8ebef4e6cb57dd9bed8b2eb3069d364661c30
David Corne <davidcorne@gmail.com>
2014-10-13 13:49:32 +0100

    Test the draw_line data.

--- test/utest_Controller.js
+++ test/utest_Controller.js
@@ -40,6 +40,38 @@ describe("Controller", function() {
         test.assert(view.users[0].colour === "red");
 
     });
+    it("emit draw line", function() {
+        var controller = new Controller.Controller();
+        var socket = new Mocks.MockSocket();
+        controller.socket = socket;
+        controller.drawLine({x: 10, y: 10}, {x: 15, y: 10});
+        test.assert(socket.emitted.length === 1);
+        test.assert(socket.emitted[0].eventName === Shared.Events.draw_line);
+        test.assert(socket.emitted[0].data !== undefined);
+        test.assert(socket.emitted[0].data.point_from.x === 10);
+        test.assert(socket.emitted[0].data.point_from.y === 10);
+        test.assert(socket.emitted[0].data.point_to.x === 15);
+        test.assert(socket.emitted[0].data.point_to.y === 10);
+
+        // test the default lineWidth/colour
+        test.assert(socket.emitted[0].data.colour === "black");
+        test.assert(socket.emitted[0].data.lineWidth === 2);
+
+        controller.lineColour = "unknown-colour";
+        controller.drawLine({x: 10, y: 10}, {x: 15, y: 10});
+        test.assert(socket.emitted.length === 2);
+        test.assert(socket.emitted[1].eventName === Shared.Events.draw_line);
+        test.assert(socket.emitted[1].data !== undefined);
+        test.assert(socket.emitted[1].data.colour === "unknown-colour");
+
+        controller.lineWidth = 14;
+        controller.drawLine({x: 10, y: 10}, {x: 15, y: 10});
+        test.assert(socket.emitted.length === 3);
+        test.assert(socket.emitted[2].eventName === Shared.Events.draw_line);
+        test.assert(socket.emitted[2].data !== undefined);
+        test.assert(socket.emitted[2].data.lineWidth === 14);
+
+    });
 });
 
 
